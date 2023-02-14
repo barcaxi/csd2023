22b6c0515025f37854204d93e402e0c841e7d3b3
Thomas D Devine <thomas@Thomass-iMac.station>
2023-02-14 19:21:22 +0000

    init

--- jQuery/AlbumsPOST.js
+++ jQuery/AlbumsPOST.js
@@ -2,13 +2,9 @@ $("document").ready(function() {
 
 	$.post("getAlbums.php", {"artist_id" : "1"}, function(data){
 		console.log(data);
-		console.log(data.albums);
-		console.log(data.albums.length);
 		$.each(data.albums, function(index, value) {	
-			console.log(value);
 			$("#myDiv").append(`${value.album_name} <br>`);
 		});
-
 	  }, "json");
   
 });
